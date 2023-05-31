bcd2b82c9872a636ef3ddf73f8c70b552fc77057
Irungu Macharia <machariajosphat001@gmail.com>
2023-05-31 17:00:58 +0000

    search bar frontend done

--- vybestore/src/App.js
+++ vybestore/src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import Header from './components/header';
 import Playlists from './components/playlists';
 import Footer from './components/footer';
-import Search from './components/search';
 
 
 function App() {
   return (
     <div className="App">
 	  <Header />
-    <Search />
 	  <Playlists />
 	  <Footer />
     </div>
--- vybestore/src/components/header.jsx
+++ vybestore/src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './header.css'
+import Search from './search.jsx'
 
 const Header = () => {
 	return (
 		<header>
 			<div className='headLogo'>
 				<span>VybeStore</span>
+				<Search />
 			</div>
 		</header>
 	)
